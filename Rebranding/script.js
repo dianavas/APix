65c069aeea97b5e470babe218c9c4a5d2cedd8a8
Diana Vasilache <diana.vasilache95@gmail.com>
2018-02-12 18:27:30 +0200

    fixed temporary webcam

--- Rebranding/script.js
+++ Rebranding/script.js
@@ -166,3 +166,22 @@ function assisInFittingRoom(){
 		break;
 	}
 }
+
+function aux(){
+var video = document.querySelector("#videoElement");
+ 
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+ 
+if (navigator.getUserMedia) {       
+    navigator.getUserMedia({video: true}, handleVideo, videoError);
+}
+ 
+function handleVideo(stream) {
+    video.src = window.URL.createObjectURL(stream);
+}
+ 
+function videoError(e) {
+    console.log("errooooooor");
+}
+}
+
